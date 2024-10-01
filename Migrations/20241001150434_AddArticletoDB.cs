c767c40305ebe0d9368192449c6b75cc51816dbe
Nikesh <subedi.nikesh@outlook.com>
2024-10-01 20:56:11 +0545

    DB migrations

---
+++ Migrations/20241001150434_AddArticletoDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArticletoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Articles");
+        }
+    }
+}
