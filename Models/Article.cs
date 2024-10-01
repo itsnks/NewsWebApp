4880050157a83867069ea3f20bca8fe06a709bfc
Nikesh <subedi.nikesh@outlook.com>
2024-10-01 13:43:16 +0545

    model for article

---
+++ Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsWebApp.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is requied!")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Content is requied")]
+        [DisplayName("Article Content")]
+        public string Content { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,20,ErrorMessage = "Order must be between 1 and 20")]
+        public int DisplayOrder { get; set; }
+    }
+}
