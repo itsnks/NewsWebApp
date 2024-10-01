60b247e46f4c1bb7f1e5b92b0c0dccafa1e1de19
Nikesh <subedi.nikesh@outlook.com>
2024-10-01 13:43:27 +0545

    dbcontext

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsWebApp.Models;
+
+namespace NewsWebApp.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet <Article> Articles { get; set; }
+    }
+}
