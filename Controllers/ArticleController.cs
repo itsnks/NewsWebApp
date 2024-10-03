5de2b3e947f1f401a302426710bcf04cd2d57c94
Nikesh <subedi.nikesh@outlook.com>
2024-10-03 20:39:10 +0545

    controller to return DB object based on the id of article clicked

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsWebApp.Data;
+using NewsWebApp.Models;
 
 namespace NewsWebApp.Controllers
 {
     public class ArticleController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDBContext _db;
+        public ArticleController(ApplicationDBContext db)
         {
-            return View();
+            _db = db;
+        }
+        public IActionResult Index(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Article? articleFromDB = _db.Articles.Find(id);
+            if (articleFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(articleFromDB);
         }
     }
 }
