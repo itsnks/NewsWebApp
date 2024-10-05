23554e633efd735a9de0f9e0404c24dc65470f55
Nikesh <subedi.nikesh@outlook.com>
2024-10-05 13:51:47 +0545

    returns view for every navbar action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,15 +19,42 @@ namespace NewsWebApp.Controllers
             _db = db;
         }
 
+//  Returns the top 20 articles as a list object based on DisplayOrder whenever index view of home controller is called
+//  Does the same for every other view by selecting the Articles based on their category.
         public IActionResult Index()
         {
-            List<Article> objArticleList = _db.Articles.OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
-            return View(objArticleList);
+            List<Article> ArticleList = _db.Articles.OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
+            return View(ArticleList);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult Politics()
+        {
+            List<Article> PoliticsArticleList = _db.Articles.Where(u => u.Category == "Politics").OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
+            return View(PoliticsArticleList);
+        }
+        public IActionResult Entertainment()
+        {
+            List<Article> EntertainmentArticleList = _db.Articles.Where(u => u.Category == "Entertainment").OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
+            return View(EntertainmentArticleList);
+        }
+        public IActionResult Sports()
+        {
+            List<Article> SportsArticleList = _db.Articles.Where(u => u.Category == "Sports").OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
+            return View(SportsArticleList);
+        }
+        public IActionResult Technology()
+        {
+            List<Article> TechnologyArticleList = _db.Articles.Where(u => u.Category == "Technology").OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
+            return View(TechnologyArticleList);
+        }
+        public IActionResult Business()
+        {
+            List<Article> BusinessArticleList = _db.Articles.Where(u => u.Category == "Business").OrderBy(obj => obj.DisplayOrder).Take(20).ToList();
+            return View(BusinessArticleList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
