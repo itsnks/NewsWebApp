0a34fe64911d78b44ae634f0e3ea12f11d3992b6
Nikesh <subedi.nikesh@outlook.com>
2024-10-23 12:49:47 +0545

    Controller for crud operations on articles

---
+++ Controllers/ArticleCrudController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsWebApp.Data;
+using NewsWebApp.Models;
+
+namespace NewsWebApp.Controllers
+{
+    public class ArticleCrudController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public ArticleCrudController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Article> ArticleList = _db.Articles.OrderBy(obj => obj.DisplayOrder).ToList();
+            return View(ArticleList);
+        }
+    }
+}
