a6fd162d59e56bebb55108ed1ea0735f7a1116f4
Nikesh <subedi.nikesh@outlook.com>
2024-11-16 16:00:08 +0545

    controller for accounts

---
+++ Controllers/Account.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsWebApp.Controllers
+{
+    public class Account : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
