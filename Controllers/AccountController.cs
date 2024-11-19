beff13edf615d52689e59744b47d484d4db507c8
Nikesh <subedi.nikesh@outlook.com>
2024-11-19 20:30:43 +0545

    added Register action

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -19,6 +19,34 @@ namespace NewsWebApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+                var result = await userManager.CreateAsync(users, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
         public IActionResult Login()
         {
             return View();
