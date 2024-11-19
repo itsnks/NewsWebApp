50ba9cd904a67462879bfe3098158f1ce8367466
Nikesh <subedi.nikesh@outlook.com>
2024-11-19 20:06:01 +0545

    added post action for user login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsWebApp.Models;
+using NewsWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace NewsWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or password is incorrect");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+    }
+}
