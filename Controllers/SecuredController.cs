178e90a064d4b057a95f55c01d39bd43a3f4a4a1
Nikesh <subedi.nikesh@outlook.com>
2024-11-22 15:43:10 +0545

    secured controller and view to test authorization

---
+++ Controllers/SecuredController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsWebApp.Controllers
+{
+    [Authorize]
+    public class SecuredController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
