3c51db080c0363e13b39151892ce6bba07cbfbf0
Nikesh <subedi.nikesh@outlook.com>
2025-01-30 22:35:21 +0545

    placed models inside Entities

---
+++ Models/Entities/Article.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewsWebApp.Models.Entities
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is requied!")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(MAX)")]
+        [Required(ErrorMessage = "Content is requied!")]
+        [DisplayName("Article Content")]
+        public string Content { get; set; }
+        [DisplayName("Is this a High Priority Article?")]
+        public bool IsPriority { get; set; }
+        public string? Author { get; set; }
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [DisplayName("Image URL (leave empty if there is no image)")]
+        public string? ImageLink { get; set; }
+    }
+}
---
+++ Models/Entities/Users.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NewsWebApp.Models.Entities
+{
+    public class Users : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
