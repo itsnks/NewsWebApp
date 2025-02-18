7ddf4b0fadba9eb3e9440bc5deb7cf5977e9a46a
Nikesh <subedi.nikesh@outlook.com>
2025-02-18 22:53:37 +0545

    viewmodel for all pages in home controller with added details on views

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,12 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using NewsWebApp.Models.Entities;
 
 namespace NewsWebApp.ViewModels
 {
     public class HomeViewModel
     {
-        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
@@ -14,31 +14,31 @@ namespace NewsWebApp.ViewModels
         public string? Author { get; set; }
         public string? Category { get; set; }
         public string? Description { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string? ImageLink { get; set; }
-        public string TimeSinceCreation { 
-            get 
-            { 
-                TimeSpan TimeElapsed = DateTime.Now - CreatedDate;
-                if (TimeElapsed.TotalSeconds < 60)
-                {
-                    return $"{(int)TimeElapsed.TotalSeconds} seconds ago";
-                }
-                if (TimeElapsed.TotalMinutes < 60)
-                {
-                    return $"{(int)TimeElapsed.TotalMinutes} minutes ago";
-                }
-                if (TimeElapsed.TotalHours < 24)
-                {
-                    return $"{(int)TimeElapsed.TotalHours} hours ago";
-                }
-                if (TimeElapsed.TotalDays < 365)
-                {
-                    return $"{(int)TimeElapsed.TotalDays} days ago";
-                }
+        public string TimeSinceCreation { get; set; }
 
-                return CreatedDate.ToString("MMMM dd, YYYY");
-            }
+        public HomeViewModel (Article article)
+        {
+            Id = article.Id;
+            Title = article.Title;
+            Content = article.Content;
+            IsPriority = article.IsPriority;
+            Author = article.Author;
+            Category = article.Category;
+            Description = article.Description;
+            CreatedDate = article.CreatedDate;
+            ImageLink = article.ImageLink;
+            TimeSpan TimeElapsed = DateTime.Now - article.CreatedDate;
+            if (TimeElapsed.TotalMinutes < 60)
+                TimeSinceCreation = $"{Math.Floor(TimeElapsed.TotalMinutes)} minutes ago";
+            else if (TimeElapsed.TotalHours < 24)
+                TimeSinceCreation = $"{Math.Floor(TimeElapsed.TotalHours)} hours ago";
+            else if (TimeElapsed.TotalHours >= 24)
+                TimeSinceCreation = $"{Math.Floor(TimeElapsed.TotalDays)} days ago";
+            else
+                TimeSinceCreation = article.CreatedDate.ToString("yyyy MMMM dd");
+
         }
-    }
+   }
 }
