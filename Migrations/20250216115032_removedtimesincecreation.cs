ff0878179ee883a682ccbd8ce59f6e20e1c06dff
Nikesh <subedi.nikesh@outlook.com>
2025-02-18 22:51:32 +0545

    more migrations

---
+++ Migrations/20250216115032_removedtimesincecreation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedtimesincecreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeSinceCreation",
+                table: "Articles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TimeSinceCreation",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
