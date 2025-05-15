eafcda9c03dfacf638f63ef0a0450bede8426a91
Nikesh <subedi.nikesh@outlook.com>
2025-05-15 15:47:31 +0545

    view model for dashboard

---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using NewsWebApp.Models.Entities;
+
+namespace NewsWebApp.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DashboardViewModel(Article article)
+        {
+            Id = article.Id;
+            Title = article.Title;
+        }
+    }
+}
