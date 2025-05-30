e20c59f8c9d9086f16aa10de16836e0a7d18af22
Nikesh <subedi.nikesh@outlook.com>
2025-05-30 14:06:43 +0545

    added parameters for pagination, searching and ordering

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -7,7 +7,11 @@ namespace NewsWebApp.ViewModels
         public IQueryable<Article> Articles { get; set; }
 
         public  string IdSortOrder { get; set; }
-
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public string Term { get; set; }
+        public string OrderBy { get; set; }
         /*        public int Id { get; set; }
                 public string Title { get; set; }
                 public DashboardViewModel(Article article)
